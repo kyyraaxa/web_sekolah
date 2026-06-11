9a544d558d83f260bd4367e5ea939779a283304c
kyyraaxa <muhammadjayadisaputra15@gmail.com>
2026-06-11 11:13:08 +0800

    commit

---
+++ resources/views/pages/category/⚡index.blade.php
@@ -0,0 +1,14 @@
+<?php
+
+use Livewire\Component;
+
+new class extends Component
+{
+    //
+};
+?>
+
+<div>
+    <h1>Hallo Cukimak</h1>
+    {{-- Very little is needed to make a happy life. - Marcus Aurelius --}}
+</div>
